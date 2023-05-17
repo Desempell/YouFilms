ef7ce55258fd1c7b58a7309a908eb7bd124c1640
Desempell <Desempell@yandex.ru>
2023-05-17 19:20:04 +0300

    Add user controller

---
+++ YouFilms/Controllers/DTO/UserDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace YouFilms.Controllers.DTO
+{
+	public class UserDTO
+	{
+		/// <summary>
+		/// Логин
+		/// </summary>
+		public string Login { get; set; }
+
+		/// <summary>
+		/// Пароль
+		/// </summary>
+		public string Password { get; set; }
+
+		/// <summary>
+		/// Путь к файлу с аватаром (?)
+		/// </summary>
+		public string Avatar { get; set; }
+
+		/// <summary>
+		/// Номер телефона
+		/// </summary>
+
+		public string? PhoneNumber { get; set; }
+
+		/// <summary>
+		/// Любимый жанр
+		/// </summary>
+		public string? FavorGenre { get; set; }
+
+		/// <summary>
+		/// Проверка на права администратора
+		/// </summary>
+		public bool IsAdministrator { get; set; }
+	}
+}
